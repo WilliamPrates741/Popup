7c03932c672b5ef820ef83901dc96827bf642f38
DavidSiqueiradosSantos <davidsiqueiradossantos556@gmail.com>
2022-05-04 21:04:41 -0300

    'Atividade'

---
+++ main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "StateLess",
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  String nome = "";
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+            appBar: AppBar(
+              title: Text("Estudando StatefulL"),
+              centerTitle: true,
+            ),
+            body: Center(
+                child: Container(
+                    width: 300,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        TextField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: "Insira o seu nome",
+                          ),
+                          onSubmitted: (String str){
+                            setState(() {
+                              nome = str;
+                            });
+                          },
+                        ),
+                        ElevatedButton(
+                            onPressed: (){
+                              setState(() {
+                                showDialog(
+                                  context: context,
+                                  builder: (_) => AlertDialog(
+                                    title: Text("Ola"),
+                                    content: Text(nome),
+                                    backgroundColor: Colors.yellow,
+
+                                  )
+
+                                    ,);
+                              });
+                            }, child: Text("Avançar")),
+                      ],
+                    )
+                )
+            )
+        )
+    );
+  }
+
+}
